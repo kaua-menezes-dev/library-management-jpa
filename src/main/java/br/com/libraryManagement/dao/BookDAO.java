15dad23f89a81c675e78cd622d91381563fb95b8
Kauã Menezes <kauavictor.f.menezes@hotmail.com>
2026-08-13 00:27:56 -0300

    feat: implementa persistência e consultas de livros (BookDAO) ao dominio

---
+++ src/main/java/br/com/libraryManagement/dao/BookDAO.java
@@ -0,0 +1,47 @@
+package br.com.libraryManagement.dao;
+
+import br.com.libraryManagement.model.entity.Book;
+import jakarta.persistence.EntityManager;
+
+import java.util.List;
+
+public class BookDAO {
+
+    private EntityManager entityManager;
+
+    public BookDAO(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public void registerBook(Book book) {
+        this.entityManager.persist(book);
+    }
+
+    public void removeBook(Book book) {
+        this.entityManager.remove(book);
+    }
+
+    public Book findBookByTitle(String bookTitle) {
+        String jpql = "SELECT b FROM Book b WHERE b.title = ?1";
+        return entityManager.createQuery(jpql, Book.class)
+                .setParameter(1, bookTitle)
+                .getSingleResult();
+    }
+
+    public Book findByBookId(Long id) {
+        return entityManager.find(Book.class, id);
+    }
+
+    public List<Book> findAllBooks() {
+        String jqpl = "SELECT b FROM Book b";
+        return entityManager.createQuery(jqpl, Book.class)
+                .getResultList();
+    }
+
+    public List<Book> findBookByAuthor(String authorName) {
+        String jpql = "SELECT b FROM Book b WHERE b.author.name = ?1";
+        return entityManager.createQuery(jpql, Book.class)
+                .setParameter(1, authorName)
+                .getResultList();
+    }
+}
